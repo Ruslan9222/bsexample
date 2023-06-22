5fed2e147ba307cc66dcc13ed497d86662274a31
Notebook <magog21@gmail.com>
2023-06-22 21:30:16 +0300

    ClassWork24

--- src/main/java/by/radevich/bsexample/HelloServlet.java
+++ src/main/java/by/radevich/bsexample/HelloServlet.java
@@ -25,6 +25,20 @@ public class HelloServlet extends HttpServlet {
         var password = request.getParameter("password");
         var currLogin = request.getServletContext().getInitParameter("currLogin");
         var currPass = request.getServletContext().getInitParameter("currPass");
+        PrintWriter pw = response.getWriter();
+        pw.println("<!doctype html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"utf-8\">\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
+                "    <title>Login form</title>\n" +
+                "    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\"\n" +
+                "          integrity=\"sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65\" crossorigin=\"anonymous\">\n" +
+                "</head>\n" +
+                "<body>");
+        pw.println("<h1>"+ login + "</h1>");
+        pw.println("<h1>"+ password + "</h1>");
+        pw.println("</body></html>");
 
         if (login.equals(currLogin)&&password.equals(currPass)){
             response.sendRedirect("success.html");
